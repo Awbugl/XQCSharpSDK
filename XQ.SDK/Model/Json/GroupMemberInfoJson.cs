9567a52cb2e5ef96f1c8223ed6f66d89fb97f0be
littlenine12 <56431872+littlenine12@users.noreply.github.com>
2021-02-21 18:21:02 +0800

    Update GroupMemberInfoJson.cs

--- XQ.SDK/Model/Json/GroupMemberInfoJson.cs
+++ XQ.SDK/Model/Json/GroupMemberInfoJson.cs
@@ -8,13 +8,13 @@ namespace XQ.SDK.Model.Json
         ///     上次发言时间时间戳
         /// </summary>
         [JsonProperty(PropertyName = "lst")]
-        public int LastTime { get; set; }
+        public long LastTime { get; set; }
 
         /// <summary>
         ///     加群时间时间戳
         /// </summary>
         [JsonProperty(PropertyName = "jt")]
-        public int JoinTime { get; set; }
+        public long JoinTime { get; set; }
 
         /// <summary>
         /// </summary>
